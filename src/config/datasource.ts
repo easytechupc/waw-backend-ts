891f98e40a5f8190b470812c5d664c429f972815
Diego Albitres <diego.albitres@gmail.com>
2022-06-16 08:46:58 -0500

    chore: setup typeorm cli datasource

---
+++ src/config/datasource.ts
@@ -0,0 +1,15 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+import "dotenv/config";
+
+export const dataSource: DataSourceOptions = {
+  type: "mysql",
+  host: process.env.DB_HOST || "127.0.0.1",
+  port: Number(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  logging: "all",
+  entities: ["./src/**/*.entity.ts"],
+};
+
+export default new DataSource(dataSource);
