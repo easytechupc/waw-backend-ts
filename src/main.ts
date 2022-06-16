ec82742af625e7f12cbdccdce825ed1015d29e5a
Diego Albitres <diego.albitres@gmail.com>
2022-06-16 04:59:20 -0500

    feat: add port customization via env var

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  const port = config.get<number>("PORT") || 3000;
+
+  await app.listen(port, () => {
+    const logger = new Logger("NestApplication");
+    logger.log(`App available on http://localhost:${port}`);
+  });
 };
 
 bootstrap();
