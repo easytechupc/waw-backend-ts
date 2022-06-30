b5467160191eca421dfe0093921f7036b01e0498
Angel Meneses <angelmenesest123@gmail.com>
2022-06-30 16:49:13 -0500

    fix: added table name

--- src/subscriptions/infrastructure/persistence/entities/plan.entity.ts
+++ src/subscriptions/infrastructure/persistence/entities/plan.entity.ts
@@ -1,6 +1,7 @@
 import { AutoMap } from "@automapper/classes";
-import { Column, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity("subscription_plans")
 export class PlanEntity {
   @AutoMap()
   @PrimaryGeneratedColumn()
