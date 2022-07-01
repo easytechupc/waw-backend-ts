f0cdb3c8cd5e2d44cfda208aac17a37fc77a6317
Angel Meneses <angelmenesest123@gmail.com>
2022-06-30 23:13:30 -0500

    fix: modified profile spec

--- src/api/subscriptions/application/transform/profiles/plan.profile.spec.ts
+++ src/api/subscriptions/application/transform/profiles/plan.profile.spec.ts
@@ -2,6 +2,6 @@ import { PlanProfile } from "./plan.profile";
 
 describe("PlanProfile", () => {
   it("should be defined", () => {
-    expect(new PlanProfile()).toBeDefined();
+    expect(PlanProfile).toBeDefined();
   });
 });
