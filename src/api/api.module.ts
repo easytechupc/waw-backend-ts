b902a3b98b77664dfc6d8a149066f57a843c724d
Kolya Castañeda <gakol8@hotmail.com>
2022-06-30 22:46:50 -0500

    feat(users): added import

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,8 +2,9 @@ import { Module } from "@nestjs/common";
 
 import { EmployersModule } from "src/api/employers/employers.module";
 import { JobsModule } from "src/api/jobs/jobs.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [EmployersModule, JobsModule],
+  imports: [EmployersModule, JobsModule, UsersModule],
 })
 export class ApiModule {}
