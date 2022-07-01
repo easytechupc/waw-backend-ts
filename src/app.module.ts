76150526ffe99eb4a9213cafc8a6cac0ccbd5b16
Diego Albitres <diego.albitres@gmail.com>
2022-06-30 19:04:16 -0500

    fix: `ApiModule` not being imported in `AppModule`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
+import { ApiModule } from "./api/api.module";
 
 import { ConfigModule } from "./config/config.module";
 import { DatabaseModule } from "./database/database.module";
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
+  imports: [ConfigModule, DatabaseModule, ApiModule],
 })
 export class AppModule {}
