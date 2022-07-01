7555fe3e9f222dda2078fbed658d6c3740c25c3e
Diego Albitres <diego.albitres@gmail.com>
2022-06-30 21:11:41 -0500

    chore: setup new TypeORM CLI settings

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,28 +1,51 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { join } from "path";
+import { DataSourceOptions } from "typeorm";
+import { SqliteConnectionOptions } from "typeorm/driver/sqlite/SqliteConnectionOptions";
+import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
+import { resolve } from "path";
 
-export const DatabaseConfig = (): TypeOrmModuleOptions => ({
-  type: process.env.NODE_ENV === "test" ? "sqlite" : "mysql",
-  extra: process.env.DB_SOCKET_PATH
-    ? {
-        socketPath: process.env.DB_SOCKET_PATH,
-      }
-    : undefined,
-  host: process.env.DB_SOCKET_PATH || process.env.DB_HOST || "localhost",
-  port: process.env.DB_SOCKET_PATH
-    ? undefined
-    : Number(process.env.DB_PORT) || 3306,
-  username: process.env.DB_SOCKET_PATH
-    ? undefined
-    : process.env.DB_USER || "root",
-  password: process.env.DB_SOCKET_PATH
-    ? undefined
-    : process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "waw",
-  entities: [join(__dirname, "../", "**/*.entity.{js,ts}")],
-  migrations: [join(__dirname, "../", "database/migrations/**/*.ts")],
-  logging: process.env.DB_LOGGING === "true",
-  synchronize: process.env.DB_SYNCHRONIZE === "true",
-  dropSchema: process.env.DB_DROP_SCHEMA === "true",
-  migrationsRun: process.env.DB_MIGRATIONS_RUN === "true",
-});
+const env = (key: string) => process.env[key];
+
+export const DatabaseConfig = (): DataSourceOptions => {
+  const base = {
+    entities: [resolve(__dirname, "../", "**/*.entity.{js,ts}")],
+    migrations: [resolve(__dirname, "../", "database/migrations/**/*.ts")],
+    logger: "advanced-console",
+    logging: ["warn", "error"],
+    synchronize: env("DB_SYNCHRONIZE") === "true",
+    dropSchema: env("DB_DROP_SCHEMA") === "true",
+    migrationsRun: env("DB_MIGRATIONS_RUN") === "true",
+  } as DataSourceOptions;
+
+  if (process.env.NODE_ENV === "test") {
+    return {
+      ...(base as SqliteConnectionOptions),
+      type: "sqlite",
+      database: env("DB_NAME") || ":memory:",
+      logging: env("DB_LOGGING") === "true",
+    };
+  }
+
+  if (process.env.DB_SOCKET_PATH) {
+    return {
+      ...(base as MysqlConnectionOptions),
+      type: "mysql",
+      database: env("DB_NAME") || "wawts",
+      host: env("DB_SOCKET_PATH"),
+      extra: {
+        socketPath: env("DB_SOCKET_PATH"),
+      },
+      username: env("DB_USER") || "root",
+      password: env("DB_PASSWORD") || "root",
+    };
+  }
+
+  return {
+    ...(base as MysqlConnectionOptions),
+    type: "mysql",
+    database: env("DB_NAME") || "wawts",
+    host: env("DB_HOST") || "127.0.0.1",
+    port: Number(env("DB_PORT")) || 3306,
+    username: env("DB_USER") || "root",
+    password: env("DB_PASSWORD") || "root",
+  };
+};
