e07b0cf6b183be215c72f2ce1a7b1b3a5d726975
Kolya Castañeda <gakol8@hotmail.com>
2022-06-30 20:57:17 -0500

    feat(users): added module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,22 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserProfile } from "./application/transform/profiles/user.profile";
+import { UsersRepositoryKey } from "./domain/repositories/users.repository";
+import { UsersService } from "./domain/services/users.service";
+import { UserEntity } from "./infraestructure/persistence/entities/user.entity";
+import { UsersRepository } from "./infraestructure/persistence/repositories/users.repository";
+import { UsersController } from "./interface/rest/users.controller";
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [
+    {
+      provide: UsersRepositoryKey,
+      useClass: UsersRepository,
+    },
+    UsersService,
+    UserProfile,
+  ],
+  controllers: [UsersController],
+})
 export class UsersModule {}
