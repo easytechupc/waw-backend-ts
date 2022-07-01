9544a33b045825e3460396d32c3fef76505955cf
Angel Meneses <angelmenesest123@gmail.com>
2022-06-30 21:58:11 -0500

    refactor: modified api module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,8 +2,9 @@ import { Module } from "@nestjs/common";
 
 import { EmployersModule } from "src/api/employers/employers.module";
 import { JobsModule } from "src/api/jobs/jobs.module";
+import { SubscriptionsModule } from "./subscriptions/subscriptions.module";
 
 @Module({
-  imports: [EmployersModule, JobsModule],
+  imports: [EmployersModule, JobsModule, SubscriptionsModule],
 })
 export class ApiModule {}
