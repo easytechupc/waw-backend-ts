d09d0016832172121378ac16c4a91896e308c0c9
Kolya Castañeda <gakol8@hotmail.com>
2022-06-30 19:06:27 -0500

    feat(users): created mapping profile

---
+++ src/users/application/transform/profiles/user.profile.ts
@@ -0,0 +1,21 @@
+import { createMap, Mapper, MappingProfile } from "@automapper/core";
+import { AutomapperProfile, InjectMapper } from "@automapper/nestjs";
+import { User } from "src/users/domain/entities/user.model";
+import { UserEntity } from "src/users/infraestructure/persistence/entities/user.entity";
+import { UserRequest } from "../resources/user.request";
+import { UserResource } from "../resources/user.resource";
+
+export class UserProfile extends AutomapperProfile {
+  constructor(@InjectMapper() mapper: Mapper) {
+    super(mapper);
+  }
+
+  get profile(): MappingProfile {
+    return mapper => {
+      createMap(mapper, User, UserResource);
+      createMap(mapper, User, UserEntity);
+      createMap(mapper, UserEntity, User);
+      createMap(mapper, UserRequest, User);
+    };
+  }
+}
